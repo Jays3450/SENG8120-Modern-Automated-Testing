70f5ba24c3fbba9889d7994c774d6f4386e4cf87
Jayashree Srinivasan <Jsrinivasan3450@conestogac.on.ca>
2025-06-15 21:39:21 -0400

    Added test case # 2 to paintRequiredCalculator - returns infinity when coverage per liter is 0

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -11,4 +11,9 @@ describe("paint required calculator", () => {
     const result = paintRequiredCalculator(0, 10);
     expect(result).toEqual(0);
   });
+  //Test case # 2
+  test("returns Infinity when coveragePerLiter is 0", () => {
+    const result = paintRequiredCalculator(100, 0);
+    expect(result).toEqual(Infinity);
+  });
 });
