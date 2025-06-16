ec75e802e0a6e8e255d5884b15b8dc430fa9bb41
Jayashree Srinivasan <Jsrinivasan3450@conestogac.on.ca>
2025-06-15 20:57:07 -0400

    Added Test case #1 - return 0 value, to both the test files

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -6,4 +6,9 @@ describe("paint required calculator", () => {
 
     expect(result).toEqual(5);
   });
+  // Test case # 1
+  test("returns 0 when area is 0", () => {
+    const result = paintRequiredCalculator(0, 10);
+    expect(result).toEqual(0);
+  });
 });
