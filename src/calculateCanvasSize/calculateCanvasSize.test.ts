0cb0563cbd72e06c2f986503ed4b5b18fd23c1ea
Jayashree Srinivasan <Jsrinivasan3450@conestogac.on.ca>
2025-06-15 21:06:28 -0400

    Added Test case # 1 - resturns Value 0 - to be th the test files

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -6,4 +6,9 @@ describe("calculateCanvasSize", () => {
 
     expect(result).toEqual(1000);
   })
+  // Test case # 1
+   test("returns 0 when one value is 0", () => {
+    const result = calculateCanvasSize("0", "50");
+    expect(result).toEqual(0);
+  });
 });
